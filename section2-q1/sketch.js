7b4ccc696102e904c820d2280e1d3bf89458133a
MA0TO <72617494+MA0TO@users.noreply.github.com>
2020-10-21 17:13:02 +0900

    Update sketch.js
    小手調べ

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,16 +1,16 @@
 // 小手調べ
 function setup(){
-  for(let i = 1; i < 11; i++){
+  for(let i = 0; i < 10; i++){
     // BLANK[1]
 noFill();
   stroke(255,0,0);
-  let x = i*10;
+  let x = (i+1)*10;
   ellipse(50,50,x, x);
 }
-for(let i = 1; i < 6; i++){
+for(let i = 0; i < 5; i++){
 noFill();
 stroke(0,0,255);
-let x = i*10;
+let x = (i+1)*10;
 ellipse(50,50,x, x);
 }
 }
