d8a1811fa067edded0aaa44867fc0672c88c7531
MA0TO <72617494+MA0TO@users.noreply.github.com>
2020-11-10 14:37:44 +0900

    update 4-q1

--- section4-q1/sketch.js
+++ section4-q1/sketch.js
@@ -9,8 +9,12 @@ function setup(){
 }
 
 function draw(){
-  background(160, 192, 255);
+  background(255, 200, 255);
   count = (count + 1) % cycle;
   // BLANK[1]
+count = constrain(count, width/3, width/2)
+let size = count
+
+fill(255,100,150)
   ellipse(width / 2, height / 2, size);
 }
