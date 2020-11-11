6a212c01589392fdda41896a1033e8bdad3cdad7
MA0TO <72617494+MA0TO@users.noreply.github.com>
2020-11-11 14:03:58 +0900

    mimamori

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -1,5 +1,5 @@
 // テキスト「アニメーション」
-let x, y, vx, vy;
+let x, y, vx, vy, d=0 , a 
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
@@ -12,6 +12,10 @@ function setup(){
 function draw(){
   background(160, 192, 255);
   // BLANK[2] (hint: 作った star 関数を使います)
+fill(50,100,255);
+star(x, y, 20, a)
+d = d + 1
+a = d
 
   // 端の処理パターン (1) 反対側から出てくる
   if(x > width){ x = 0; }
@@ -22,9 +26,11 @@ function draw(){
 
 function star(cx, cy, r, angle){
   beginShape();
+  noStroke();
   for(var i = 0; i < 20; i++){
-    var theta = TWO_PI * i * 2 / 5 - HALF_PI;
+    var theta = TWO_PI * i * 2 / 5 - HALF_PI * angle;
     // BLANK[1] (hint: angle 分だけ星を回転させるには？)
+
     var x = cx + cos(theta) * r;
     var y = cy + sin(theta) * r;
     vertex(x,y);
