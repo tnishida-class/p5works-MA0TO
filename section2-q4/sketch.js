33a4e7096622ba20a2b67317952bd0d208c30cec
MA0TO <72617494+MA0TO@users.noreply.github.com>
2020-11-12 13:06:12 +0900

    update2-q4

--- section2-q4/sketch.js
+++ section2-q4/sketch.js
@@ -1,5 +1,5 @@
 // スイス国旗
-// function setup() {
+//function setup() {
 //   createCanvas(270, 210);
 //   noStroke();
 //   background(255,0,0);
@@ -11,17 +11,20 @@
 
 
 // // ギリシャ国旗
-  const blue = color(0, 51, 160);
+function setup() {
+  const blue = color(0, 90, 160);
   createCanvas(270, 180);
-  // noStroke();
+   noStroke();
   background(255);
 
   let d = height / 9; // 縞1本の太さ
 
   for(let i = 0; i < 9; i++){
     // BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
-    rect(0, i * d, width, (i + 1) * d);
-  }
+    if(i % 2 == 0){fill(blue)
+  rect(0, i * d, width, d);
+}
+}
 
   fill(blue);
   let size = d * 5;
@@ -29,4 +32,6 @@
 
   fill(255);
   // BLANK[2] (hint: 白い十字を描くには rect を二つ描こう)
+  rect(0, 2*d, d*5, d)
+  rect(2*d, 0, d, d*5)
 }
