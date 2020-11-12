c122c6ebe5e556299aea21e666349dec897f6b5a
MA0TO <72617494+MA0TO@users.noreply.github.com>
2020-11-12 17:23:08 +0900

    update4-q2秀コース課題

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -26,10 +26,14 @@ x=x+vx
 y=y+vy
 
   // 端の処理パターン (1) 反対側から出てくる
-  if(x > width){ x = 0; }
-  else if(x < 0){ x = width; }
-  if(y > height){ y = 0; }
-  if(y < 0){ y = height; }
+//   if(x > width){ x = 0; }
+//   else if(x < 0){ x = width; }
+//   if(y > height){ y = 0; }
+//   if(y < 0){ y = height; }
+// }
+// 端の処理パターン (2) 跳ね返る
+if(x < 0 || x > width){ vx = -1 * vx; }
+  if(y < 0 || y > height){ vy = -1 * vy; }
 }
 
 function star(cx, cy, r, angle){
