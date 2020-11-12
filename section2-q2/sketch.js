0372dd19445d86385a69352344851d643bfaf0d2
MA0TO <72617494+MA0TO@users.noreply.github.com>
2020-11-12 13:52:55 +0900

    update2-q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -1,31 +1,22 @@
 // チェッカー
 function setup() {
-  createCanvas(200, 200);
+  createCanvas(400, 400);
   let size = width / 8;
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
-if((i+j)%2==1){
-fill(128)
-    rect(size*i,size*j,size,size)
-  }
-}
-}
-  for(let i = 0; i<8; i++){
-    for(let j =0; j<3; j++){
-      if((i+j)%2==1){
-        fill(255,0,0)
-        ellipse(size*i+size/2,size*j+size/2,size,size)
-      }
-    }
-    }
-    for(let i = 0; i<8; i++){
-      for(let j =7; 4<j; j--){
-        if((i+j)%2==1){
-          fill(0)
-          ellipse(size*i+size/2,size*j+size/2,size,size)
-}
+  if((i+j)%2==1){
+    fill(128)
+    rect(size*i,size*j,size,size)}
+
+  if((i+j)%2==1 && j<3 ){
+    fill(255,0,0)
+    ellipse(size*i+size/2,size*j+size/2,size*0.85,size*0.85)}
+
+  if((i+j)%2==1 && 4<j){
+    fill(0)
+    ellipse(size*i+size/2,size*j+size/2,size*0.85,size*0.85)}
 }
 }
 }
