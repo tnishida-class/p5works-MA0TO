e283a1601781d6c2a8472eb2aadb87924139c6b4
MA0TO <72617494+MA0TO@users.noreply.github.com>
2020-11-12 13:18:59 +0900

    update2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -3,30 +3,23 @@ function setup(){
   for(let i = 0; i < 10; i++){
     // BLANK[1]
 
-  　　　　　　　　　　//for2回使うパターン！
+  　　　　　　　　　　//for2回使うパターン
 // noFill();
 //   stroke(255,0,0);
 //   let x = (i+1)*10;
-//   ellipse(50,50,x, x);
-// }
-// for(let i = 0; i < 5; i++){
-// noFill();
+//   ellipse(50,50,x, x);}
+// for(let i = 0; i < 5; i++){noFill();
 // stroke(0,0,255);
 // let x = (i+1)*10;
-// ellipse(50,50,x, x);
-// }
+// ellipse(50,50,x, x);}
 // }
 
-　　　　　　　　　　　// ifで書くパターン！
+　　　　　　　　　　　// ifで書くパターン（書き直し）
  noFill();
 let x = (i+1)*10;
-if(i<5){
-stroke(0,0,255);
- ellipse(50,50,x, x);
-}
-else{
-  stroke(255,0,0);
-   ellipse(50,50,x, x);
-}
+if(i<5){stroke(0,0,255);
+ ellipse(50,50,x, x);}
+else{stroke(255,0,0);
+   ellipse(50,50,x, x);}
 }
 }
